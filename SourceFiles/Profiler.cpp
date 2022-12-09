5ba5542b9b0e68a7944b2fdf4a375c5cdd1c8b38
Viacheslav <lslava121001l@gmail.com>
2022-12-09 10:42:08 +0300

    add norm print profiler

--- SourceFiles/Profiler.cpp
+++ SourceFiles/Profiler.cpp
@@ -10,7 +10,7 @@ private:
     chrono::steady_clock::time_point start_time;
 
 public:
-    Profiler()
+    Profiler(string name)
     {
         start_time = chrono::steady_clock::now();
     }
@@ -19,6 +19,6 @@ public:
     {
         auto end_time = chrono::steady_clock::now();
         auto delta_time = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
-        cout << delta_time << endl;
+        cout << name << ": " << delta_time << endl;
     }
 };
